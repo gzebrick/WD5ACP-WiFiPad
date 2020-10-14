474042e810ac71efb64eed2676ea263e083337d8
gzebrick <63311374+gzebrick@users.noreply.github.com>
2020-10-13 20:36:58 -0500

    Add files via upload
    See release notes on GitHub

---
+++ settings.h
@@ -0,0 +1,32 @@
+// Setup
+#define WIFI_SSID "zhome"
+#define WIFI_PASS ""
+#define WIFI_HOSTNAME "WD5ACP_WiFiPad"
+
+#define Host_IP "192.168.1.89"
+#define Host_PORT 60000
+
+// Depending upon programming requirements - modify code as needed to assure proper connection. Default is Kenwood TS890 Admin login
+#define Host_USER "WD5ACP"
+#define Host_PASS "WD5ACP"
+#define Host_ID "" // Alternative to using user name and password. Must type complete ID string. Ignored if not just ""
+
+// Pins for the ILI9341
+#define TFT_DC D2
+#define TFT_CS D1
+#define TFT_LED D8
+
+#define HAVE_TOUCHPAD
+#define TOUCH_CS D3
+#define TOUCH_IRQ  D4
+
+const int ON_AIR_LIMIT = 600000; // 10 minute timer
+const int KEEP_ALIVE_TIME = 4000; // secs * 1000 to tickle host to keep connection alive
+const int SLEEP_INTERVAL_SECS = 7200;
+
+
+
+
+/***************************
+ * End Settings
+ **************************/
