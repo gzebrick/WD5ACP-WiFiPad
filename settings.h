aac25b5d6908b48d7daa2659d6cefda940acd14a
gzebrick <63311374+gzebrick@users.noreply.github.com>
2020-10-15 07:14:28 -0500

    Add files via upload

--- settings.h
+++ settings.h
@@ -1,17 +1,25 @@
-// Setup
-#define WIFI_SSID "zhome"
+// Setup -Modify as needed per QTH  WiFi and Host --------------------------------------------------------------------
+#define WIFI_SSID "zhome"            
 #define WIFI_PASS ""
 #define WIFI_HOSTNAME "WD5ACP_WiFiPad"
 
-#define Host_IP "192.168.1.89"
-#define Host_PORT 60000
-
-// Depending upon programming requirements - modify code as needed to assure proper connection. Default is Kenwood TS890 Admin login
+// This is the configuration to match the Admin user on the host. Default is for Kenwood TS-890 ----------------------
+#define Host_IP "192.168.1.89"  // This is the static/fixed/non-changing IP address of the Host. 
+#define Host_PORT 60000  // TCP Port 60000 required for Kenwood TS-890
 #define Host_USER "WD5ACP"
 #define Host_PASS "WD5ACP"
-#define Host_ID "" // Alternative to using user name and password. Must type complete ID string. Ignored if not just ""
+#define Host_ID "" // Alternative to using user name and password. Must type complete ID string. This is ignored if not just ""
 
-// Pins for the ILI9341
+// Program display config and  timers - change as desired. Note all timers are in SECONDs -----------------------------
+//                  "..............." Message is centered in display above "ON AIR" 
+#define ON_AIR_CALL "WD5ACP"           // 15 chars max!
+const int ON_AIR_LIMIT = 600; // 10 minute timer 600000 = 10 mins
+const int KEEP_ALIVE_TIME = 4; // secs to tickle host to keep connection alive
+const int SLEEP_INTERVAL_SECS = 7200;  // in seconds - turns off backlight - touch screen to wake up
+
+
+ 
+// Pins for the ILI9341  DO NOT CHANGE -------------------------------------------------------------------------------
 #define TFT_DC D2
 #define TFT_CS D1
 #define TFT_LED D8
@@ -19,13 +27,6 @@
 #define HAVE_TOUCHPAD
 #define TOUCH_CS D3
 #define TOUCH_IRQ  D4
-
-const int ON_AIR_LIMIT = 600000; // 10 minute timer
-const int KEEP_ALIVE_TIME = 4000; // secs * 1000 to tickle host to keep connection alive
-const int SLEEP_INTERVAL_SECS = 7200;
-
-
-
 
 /***************************
  * End Settings
