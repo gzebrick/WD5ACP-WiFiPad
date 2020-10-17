21ed2e469682456af9db1adcc1714344e53427b8
gzebrick <63311374+gzebrick@users.noreply.github.com>
2020-10-17 13:35:23 -0500

    Add files via upload
    Ver 5 with separate config settings h files for each of 4 custom button screens.

---
+++ ConfigMenu2.h
@@ -0,0 +1,86 @@
+// Button Label2s should NOT include the ';' end of line charachter
+// but any valid Macro2 commands sent  out require it.
+// Up to 4 Macro2 commands may be used per button - if NOT required, set unused Macro2 command to ""
+//
+// Example
+// #define Label261 "RIT OFF"   // Button 6, Label2 1 (top Label2) about 12 char max
+// #define Label262 " & CLEAR"  // Button 6, Label2 2 (bottom Label2) about 12 char max
+// #define Macro261 "RT0;"      // Button 6, Macro2 command 1, must terminate with ';' char
+// #define Macro262 "RC;"       // Button 6, Macro2 command 2, must terminate with ';' char
+// #define Macro263 ""          // Button 6, Macro2 command 3, unused and ignored
+// #define Macro264 ""          // Button 6, Macro2 command 4, unused and ignored
+//
+
+// Top  Menu Button
+#define Label201 "WiFi Pad"
+#define Label202 "Memory Channel Menu"
+
+// Button 1
+#define Label211 "Mem CH 51"
+#define Label212 "> VFO A"
+#define Macro211 "MN051;"
+#define Macro212 "SV;"
+#define Macro213 ""
+#define Macro214 ""
+
+// Button2
+#define Label221 "Mem CH 52"
+#define Label222 "> VFO A"
+#define Macro221 "MN052;"
+#define Macro222 "SV;"
+#define Macro223 ""
+#define Macro224 ""
+
+// Button3
+#define Label231 "Mem CH 53"
+#define Label232 "> VFO A"
+#define Macro231 "MN053;"
+#define Macro232 "SV;"
+#define Macro233 ""
+#define Macro234 ""
+
+// Button4
+#define Label241 "Mem CH 54"
+#define Label242 "> VFO A"
+#define Macro241 "MN054;"
+#define Macro242 "SV;"
+#define Macro243 ""
+#define Macro244 ""
+
+// Button5
+#define Label251 "Mem CH 55"
+#define Label252 "> VFO A"
+#define Macro251 "MN055;"
+#define Macro252 "SV;"
+#define Macro253 ""
+#define Macro254 ""
+
+// Button6
+#define Label261 ""
+#define Label262 ""
+#define Macro261 ""
+#define Macro262 ""
+#define Macro263 ""
+#define Macro264 ""
+
+// Button7
+#define Label271 ""
+#define Label272 ""
+#define Macro271 ""
+#define Macro272 ""
+#define Macro273 ""
+#define Macro274 ""
+
+// Button8
+#define Label281 ""
+#define Label282 ""
+#define Macro281 ""
+#define Macro282 ""
+#define Macro283 ""
+#define Macro284 ""
+
+
+
+/***************************
+   End Settings
+ **************************/
--- settings.h
+++ settings.h
@@ -1,24 +1,20 @@
-// Setup -Modify as needed per QTH  WiFi and Host --------------------------------------------------------------------
-#define WIFI_SSID "zhome"            
-#define WIFI_PASS ""
-#define WIFI_HOSTNAME "WD5ACP_WiFiPad"
+// required MaxMenus setting. Please onfigure max number of active menus 0-4. 
+// MaxMenus = 0 means ONLY the ON-AIR timer funciton is active
+// MaxMenus 1-4 will display macro push buttons menus. Menu 0 is the default view
+// EVERY MENU requires a correstponding ConfigMenu settings. Configure each custom menu by editing corresponding ConfigMenu.h files
 
-// This is the configuration to match the Admin user on the host. Default is for Kenwood TS-890 ----------------------
-#define Host_IP "192.168.1.89"  // This is the static/fixed/non-changing IP address of the Host. 
-#define Host_PORT 60000  // TCP Port 60000 required for Kenwood TS-890
-#define Host_USER "WD5ACP"
-#define Host_PASS "WD5ACP"
-#define Host_ID "" // Alternative to using user name and password. Must type complete ID string. This is ignored if not just ""
+const int MaxMenus = 2;  // Set this number to match the quantity of ConfigMenu.h files you configured. This controls the menu rotation 
 
 // Program display config and  timers - change as desired. Note all timers are in SECONDs -----------------------------
 //                  "..............." Message is centered in display above "ON AIR" 
 #define ON_AIR_CALL "WD5ACP"           // 15 chars max!
-const int ON_AIR_LIMIT = 600; // 10 minute timer 600000 = 10 mins
+const int ON_AIR_LIMIT = 600; // 10 minute timer 600 = 10 mins
 const int KEEP_ALIVE_TIME = 4; // secs to tickle host to keep connection alive
 const int SLEEP_INTERVAL_SECS = 7200;  // in seconds - turns off backlight - touch screen to wake up
 
 
- 
+
+
 // Pins for the ILI9341  DO NOT CHANGE -------------------------------------------------------------------------------
 #define TFT_DC D2
 #define TFT_CS D1
